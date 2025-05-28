10584cfa6d10920ec1e1899e533671dad5492014
srideep9 <dornala.srideep@gmail.com>
2025-05-27 17:40:05 -0700

    finished whole lab

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Lab8-Starter
+
+Name: Srideep Dornala
+
+Explore: How are graceful degradation and service workers related?
+
+Graceful degradation and service workers are related because graceful degradation is the opposite of progressive enhancement, meaning that resilience, performance, and accesibility must be added over time. In this case, we created network requests first, and are adding service workers to improve resilience so that the application we create is not super dependent on the network which we cannot control. By implementing service workers to reduce dependency on the internet, we are partaking in graceful degradation when developing the website.
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -45,6 +45,20 @@ function initializeServiceWorker() {
   // We first must register our ServiceWorker here before any of the code in
   // sw.js is executed.
   // B1. TODO - Check if 'serviceWorker' is supported in the current browser
+  if('serviceWorker' in navigator){
+    window.addEventListener('load', async () =>{
+      try {
+        const registration = await navigator.serviceWorker.register("/sw.js", {
+          scope: "/",
+        });
+        if (registration.active) {
+          console.log("Service worker active");
+        }
+      } catch (error) {
+        console.error(`Registration failed with ${error}`);
+      }
+    });
+  }
   // B2. TODO - Listen for the 'load' event on the window object.
   // Steps B3-B6 will be *inside* the event listener's function created in B2
   // B3. TODO - Register './sw.js' as a service worker (The MDN article
@@ -67,16 +81,37 @@ function initializeServiceWorker() {
 async function getRecipes() {
   // EXPOSE - START (All expose numbers start with A)
   // A1. TODO - Check local storage to see if there are any recipes.
-  //            If there are recipes, return them.
+  //            If there are recipes, return them.\
+  if (localStorage.getItem('recipes') != null){
+    return JSON.parse(localStorage.getItem('recipes'));
+  }
   /**************************/
   // The rest of this method will be concerned with requesting the recipes
   // from the network
   // A2. TODO - Create an empty array to hold the recipes that you will fetch
+  let recipeArray = []
   // A3. TODO - Return a new Promise. If you are unfamiliar with promises, MDN
   //            has a great article on them. A promise takes one parameter - A
   //            function (we call these callback functions). That function will
   //            take two parameters - resolve, and reject. These are functions
   //            you can call to either resolve the Promise or Reject it.
+  return new Promise(async (resolve, reject) => {
+
+    for(let i = 0; i < RECIPE_URLS.length; i++){
+      try {
+        const urlFetch = await fetch(RECIPE_URLS[i]);
+        const jsonCall = await urlFetch.json();
+        recipeArray.push(jsonCall);
+        if (i == RECIPE_URLS.length - 1){
+          saveRecipesToStorage(recipeArray);
+          resolve(recipeArray);
+        }
+      } catch (error) {
+        console.error("error: ", error);
+        reject(error)
+      }
+    }
+  });
   /**************************/
   // A4-A11 will all be *inside* the callback function we passed to the Promise
   // we're returning
